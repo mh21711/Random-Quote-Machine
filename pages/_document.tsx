492853f59222370a7e8f8414ddeed28ae108c521
MahmoudElrashedy <elrashedymahmoud@gmail.com>
2024-03-21 16:52:09 +0200

    Initial Commit

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,7 +9,7 @@ export default function Document() {
       <body>
         <Main />
         <NextScript />
-        <script src="https://cdn.freecodecamp.org/testable-projects-fcc/v1/bundle.js"></script>
+        <script src="https://cdn.freecodecamp.org/testable-projects-fcc/v1/bundle.js" async></script>
       </body>
     </Html>
   );
